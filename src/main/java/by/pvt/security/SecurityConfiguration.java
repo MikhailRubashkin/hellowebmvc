9497631172ee097e9dd71d7bfd9c1c4c6b617e78
rubashkin_mikhail@tut.by <pristijpribor@mail.ru>
2019-10-15 22:58:49 +0200

    +security

---
+++ src/main/java/by/pvt/security/SecurityConfiguration.java
@@ -0,0 +1,45 @@
+package by.pvt.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        // enable in memory based authentication with a user named
+        // "user" and "admin"
+        auth.inMemoryAuthentication()
+                .withUser("user").password("{noop}password").roles("USER").and()
+                .withUser("admin").password("{noop}password").roles("USER", "ADMIN");
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        super.configure(web);
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/add-product")
+                .hasRole("ADMIN")
+                .antMatchers(HttpMethod.POST, "/add-product")
+                .hasRole("ADMIN")
+                .and()
+                .formLogin()
+                .and()
+                .csrf().disable()
+                .sessionManagement()
+                .sessionFixation().none();
+    }
+
+
+}
--- src/main/webapp/WEB-INF/jsp/searchResult.jsp
+++ src/main/webapp/WEB-INF/jsp/searchResult.jsp
@@ -1,5 +1,6 @@
 <%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
 <jsp:include page="header.jsp"/>
+
 <table class="table">
     <thead>
     <tr>
